9750459e6904cd20419abe2ab9c03889d680e6cc
Saajan Shrestha <shresthasaajan31@gmail.com>
2022-08-17 11:41:42 +0545

    Add: List all contact page

--- src/routes/RoutesPath.tsx
+++ src/routes/RoutesPath.tsx
@@ -1,7 +1,7 @@
-import { Link } from "react-router-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Logout from "../components/Logout/Logout";
 import { HOME, LOGOUT, SIGN_IN, SIGN_UP } from "../constants/routesConstants";
+import Home from "../pages/Home/Home";
 import SignIn from "../pages/Signin/SignIn";
 import SignUp from "../pages/Signup/SignUp";
 import AuthenticatedRoutes from "./AuthenticatedRoute";
@@ -11,20 +11,7 @@ export const RoutesPath = () => {
         <BrowserRouter>
             <Routes>
                 <Route path={HOME} element={<AuthenticatedRoutes />}>
-                    <Route
-                        index
-                        element={
-                            <div>
-                                Home
-                                <Link
-                                    to={LOGOUT}
-                                    className="text-decoration-none"
-                                >
-                                    Logout
-                                </Link>
-                            </div>
-                        }
-                    />
+                    <Route index element={<Home />} />
                     <Route path={LOGOUT} element={<Logout />} />
                 </Route>
                 <Route path={SIGN_IN} element={<SignIn />} />
